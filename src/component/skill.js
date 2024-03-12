a8621bbc8e13ba85818230412e177bfa032e1d88
sagar shinde <shindesagar01677@gmail.com>
2024-03-12 12:00:04 +0530

    update file

--- src/component/skill.js
+++ src/component/skill.js
@@ -7,10 +7,42 @@ export default function Skill() {
     <div className='Skill-container'>
         <h2>My Skills</h2>
         <div className='progress-container'>
-          <span>HTML</span>
+          <span>Html</span>
           <div className='my-progress'>
-            <div className='progress'>
+            <div className='progress' id='html'>
               80%
+            </div>
+          </div>
+        </div>
+        <div className='progress-container'>
+          <span>Css</span>
+          <div className='my-progress'>
+            <div className='progress' id='css'>
+              90%
+            </div>
+          </div>
+        </div>
+        <div className='progress-container'>
+          <span>Java Script</span>
+          <div className='my-progress'>
+            <div className='progress' id='js'>
+              75%
+            </div>
+          </div>
+        </div>
+        <div className='progress-container'>
+          <span>Bootstrap</span>
+          <div className='my-progress'>
+            <div className='progress' id='bootstrap'>
+              85%
+            </div>
+          </div>
+        </div>
+        <div className='progress-container'>
+          <span>React-Js</span>
+          <div className='my-progress'>
+            <div className='progress' id='react'>
+              70%
             </div>
           </div>
         </div>
